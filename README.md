7bc185c60f86e470d18f0b93b2fc466ea10a4115
AlesLearningCode <146716296+AlesLearningCode@users.noreply.github.com>
2023-10-31 20:35:08 +0100

    Add a delete button and change read status
    Did some more work on delete button changing code of an array in JS

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # library
+
+//add name to a dom element attribute
+//search index in DOM with map by it's value
+
--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,67 @@
+const myLibrary = [];
+
+
 const displayBooks = document.querySelector(`.Book`)
 const addBook = document.querySelector(`.addbook`)
 const newAuthor = document.querySelector(`.author`)
 const newTitle = document.querySelector(`.title`)
 const newPages = document.querySelector(`.pages`)
-const newRead = document.querySelector(`.read`)
+const newReads = document.querySelectorAll(`input[name="read"]`)
 const dialog = document.querySelector(`.dialog`)
 const newBook = document.querySelector(`.newbook`)
 
-const myLibrary = [];
-myLibrary.push(new Book(`The Hobbit`, `J.R.R. Tolkien`, 295, `not read yet`))
-myLibrary.push(new Book(`The Hobbit`, `J.R.R. Tolk`, 295, `not read yet`))
+
+function createBook(){
+  for(let i = myLibrary.length -1; i < myLibrary.length; i++){
+  const displayBook = document.createElement(`div`)
+  const deleteBtn = document.createElement(`button`)
+  const changeBtn = document.createElement(`button`)
+  displayBook.setAttribute("title",`${title}`)
+  displayBooks.appendChild(displayBook)
+  node = document.createElement(`p`)
+  displayBook.appendChild(node)
+  node2 = document.createElement(`p`)
+  displayBook.appendChild(node2)
+  node3 = document.createElement(`p`)
+  displayBook.appendChild(node3)
+  node4 = document.createElement(`p`)
+  node4.setAttribute("title",`${title}`)
+  displayBook.appendChild(node4)
+  deleteBtn.textContent = `Delete`
+  changeBtn.textContent = `Change read status`
+  displayBook.appendChild(changeBtn)
+  displayBook.appendChild(deleteBtn)
+  changeBtn.addEventListener('click', () =>{
+    let index = displayBook.getAttribute("title")
+    const newIndex = myLibrary.map(object => object.title).indexOf(index);
+      if(myLibrary[newIndex].read == `yes`){
+        myLibrary[newIndex].read = `no`
+        node4 = document.querySelector(`p[title="${myLibrary[newIndex].title}"]`)
+        console.log(node4)
+        node4.textContent = `Read yet?: no`
+      }else{
+        node4 = document.querySelector(`p[title="${myLibrary[newIndex].title}"]`)
+        myLibrary[newIndex].read = `yes`
+        node4.textContent = `Read yet?: yes`
+      }
+    })
+  
+  deleteBtn.addEventListener(`click`, ()=>{ 
+    let index = displayBook.getAttribute("title")
+    const newIndex = myLibrary.map(object => object.title).indexOf(index);
+    myLibrary.splice(newIndex, 1)
+    displayBooks.removeChild(displayBook)
+    console.log(newIndex)
+    })
+  node.textContent = `Title: ${myLibrary[i].title}`
+  node2.textContent = `Author: ${myLibrary[i].author}`
+  node3.textContent = `Pages: ${myLibrary[i].pages}`
+  node4.textContent = `Read yet?: ${myLibrary[i].read}`    
+  }
+}
+
+
+
 
 newBook.addEventListener(`click`, () =>{
   dialog.showModal()
@@ -17,11 +69,10 @@ newBook.addEventListener(`click`, () =>{
 
 addBook.addEventListener(`click`, () =>{
   addBookToLibrary()
-  displayBook()
-  newTitle.value = " "
-  newAuthor.value = " "
-  newPages.value = " "
-  newRead.value = " "
+  createBook()
+  newTitle.value = ""
+  newAuthor.value = ""
+  newPages.value = ""
   dialog.close()
   console.log(myLibrary)
 })
@@ -42,35 +93,33 @@ function addBookToLibrary(){
   title = newTitle.value
   author = newAuthor.value
   pages = newPages.value
-  read = newRead.value
+for (const newRead of newReads) {
+  if (newRead.checked) {
+    read = newRead.value;
+    break;
+  }
+}
   myLibrary.push(new Book(title, author, pages, read))
 }
 
-displayBook()
-function displayBook(){
-for(let books in myLibrary){
-  let displayBook = document.createElement(`div`)
-  displayBooks.appendChild(displayBook)
-  node = document.createElement(`p`)
-  displayBook.appendChild(node)
-  node2 = document.createElement(`p`)
-  displayBook.appendChild(node2)
-  node3 = document.createElement(`p`)
-  displayBook.appendChild(node3)
-  node4 = document.createElement(`p`)
-  displayBook.appendChild(node4)
-  node.textContent = `Title: ${myLibrary[books].title}`
-  node2.textContent = `Author: ${myLibrary[books].author}`
-  node3.textContent = `Pages: ${myLibrary[books].pages}`
-  node4.textContent = `Read yet?: ${myLibrary[books].read}`
-  allbooks = [myLibrary]
-  delete myLibrary[books]
+
+
+
+
+function toggle(){
+
 }
-}
+
 
 window.onload=function() {
     document.getElementById("myform").onsubmit=function(e) {
-      event.preventDefault(); // first statement
+      e.preventDefault(); // first statement
       // do something with the form field
     }
   }
+for (let books in myLibrary){
+  newLibrary = myLibrary[books]
+}
+console.log(myLibrary)
+console.log(displayBook)
+
